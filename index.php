ff77c0d5aaf5b927a78ba0bcdadf3ec762dc299e
mayer <bmnyor@gmail.com>
2021-05-04 19:47:55 -0300

    sending files

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Curriculum</title>
+        <link rel="stylesheet" href="styles.css"/>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div class="container">
+            <div class="cabecalho">
+                <div class="nome">
+                    <h2>Brenno Mayer</h2>
+                    <h5>Brasileiro, 24 anos, Solteiro</h5>
+                </div>
+                <div class="info">
+                    <div class="divisor"></div>
+                    <p>CPF: 461.169.558-19</p>
+                    <p>Rua Doutor Neto de Araújo, 187, Apt 13 | Vila Mariana | 04111-000 | São Paulo, São Paulo.</p>
+                    <p>E-mail: bmayermayer40@gmail.com</p>
+                    <p>Celular: (11) 95636-2572</p>
+                </div>
+            </div>
+            <div class="content">
+                <div>
+                    <h3>Objetivo</h3>
+                    <p>Estou a procura de adquirir conhecimento na área de tecnologia para conquistar crescimento profissional  e construir uma carreira bem sucedida.</p>
+                </div>
+                <div>
+                    <h3>Resumo profissional</h3>
+                    <h4>Quality Tecnologia & Sistemas Ltda.</h4>
+                    <h5>Estágio: Maio/2018 - Dezembro/2019</h5>
+                    <p>Atuei como desenvolvedor em diversos projetos internos da organização, o que me proporcionou conquistar conhecimentos em PHP, Javascript(jQuery e Vue), Typescript e frameworks Web PHP como Phalcon e Laravel.
+                        Prestei suporte à aplicações Mobile desenvolvidas em frameworks híbridos (Nativescript e Cordova) utilizando Javascript e Typescript.</p>
+                    <h5>Trainee: Janeiro/2020 - Atualmente</h5>
+                    <p>Além do que já exercia como estagiário, atuo como desenvolvedor mobile e faço manutenção nos aplicativos já lançados pela empresa. Participo de reuniões com superiores 
+                        para planejar requisitos e questões técnicas a respeito de novos projetos relacionados aos produtos que mantenho.</p>
+                </div>
+                <div>
+                    <h3>Formação</h3>
+                    <h5 class="formacao-subtitle">Dez/2019 - </h5><p>&nbspGraduação em Sistemas da Informação, Universidade Presbiteriana Mackenzie.</p></div>
+            </div>
+        </div>
+    </body>
+</html>
